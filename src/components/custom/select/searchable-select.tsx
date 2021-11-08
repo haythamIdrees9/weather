83c79cbc50493203ca71931a937eb8225b03b76f
haythamIdrees9 <haythamidrees9@gmail.com>
2021-11-08 22:54:08 +0300

    UI show the selected value, and give user feedback as placeholder

--- src/components/custom/select/searchable-select.tsx
+++ src/components/custom/select/searchable-select.tsx
@@ -1,13 +1,13 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
 import "./seachable.select.scss"
-function Select(props: { noOptionMessage: string, width: number, height: number, options: string[], onChange: (value: any) => void }) {
+function Select(props: { placeholder:string,noOptionMessage: string, width: number, height: number, options: string[], onChange: (value: any) => void }) {
     const [opened, setOpened] = useState<boolean>(false);// true if select is opened
     const [closed, setClosed] = useState<boolean>(false);// true if select opened then closed
     const [options, setOptions] = useState<string[]>([]);// list of options that will be filtered on user input
     const [InputRef, setInputRef] = useState<React.RefObject<any>>(React.createRef());
     const [componentRef, setComponentRef] = useState<React.RefObject<any>>(React.createRef());
-    let timeOut: NodeJS.Timeout;
+    const [selectedOption, setSelectedOption] = useState<string>("");
 
     function handleChange(event: any) {
         let userInput = event?.target?.value;
@@ -15,12 +15,14 @@ function Select(props: { noOptionMessage: string, width: number, height: number,
             setOptions(props.options);
             return;
         }
-        clearTimeout(timeOut);
+        let options = props.options.filter(ele => ele.toLowerCase().includes(userInput.toLowerCase()))
+        setOptions(options);
+    }
 
-        timeOut = setTimeout(() => {
-            let options = props.options.filter(ele => ele.toLowerCase().includes(userInput.toLowerCase()))
-            setOptions(options);
-        }, 250);
+    function onOptionSelected(option: string) {
+        setSelectedOption(option);
+        setOpened(false);
+        onClosed();
     }
 
     useEffect(() => {
@@ -44,12 +46,12 @@ function Select(props: { noOptionMessage: string, width: number, height: number,
     /**
      * handle actions on close select
      */
-    function onClosed(){
+    function onClosed() {
         // handle action after closing the select to enhance the User experience 
-        setTimeout(()=>{
+        setTimeout(() => {
             InputRef.current.value = "";
             setOptions([...props.options])
-        },350)
+        }, 350)
     }
 
     /**
@@ -70,17 +72,26 @@ function Select(props: { noOptionMessage: string, width: number, height: number,
     return (
         <div className={"container f-row j-center " + ((opened) ? 'opened' : (closed) ? 'closed' : '')} ref={componentRef} >
             <div className="select br-4px b-black-1px" style={{ width: props.width + 'px', height: props.height + 'px' }} onClick={toggle}>
+                {selectedOption ?
+                    <div className="option-selected">
+                        {selectedOption}
+                    </div>
+                    :
+                    <div className="option-not-selected">
+                        {props.placeholder}
+                    </div>
+                    }
                 <img src='/images/arrow-up.svg' alt="arrow up" />
             </div>
             <div className="options br-4px" style={{ top: (props.height + 4) + 'px' }}>
                 <div className="input-container">
-                    <input type="text" name="filter" onChange={handleChange} ref={InputRef} />
+                    <input type="text" name="filter" onChange={handleChange} ref={InputRef} placeholder="filter..." />
                 </div>
                 <div className="options-list" >
 
                     {
                         options.length > 0 ?
-                            options.map(option => <div className="option"> {option} </div>)
+                            options.map(option => <div className="option" onClick={() => onOptionSelected(option)}> {option} </div>)
                             : <div>{props.noOptionMessage}</div>
                     }
 
