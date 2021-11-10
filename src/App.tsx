3ecffabcc7e4ba27b25be778f1e135eabaa1bf18
haythamIdrees9 <haythamidrees9@gmail.com>
2021-11-10 22:00:15 +0300

    use color as background instead of image, and generaliz the scroll style

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import Weather from './components/weather/weather';
 function App() {
   return (
     <div className="App f-col a-center" >
-      <div className="img-br" style={{ background: 'url(/bg.jpg) no-repeat' }}></div>
       <div className="weather-container">
         <Weather />
       </div>
