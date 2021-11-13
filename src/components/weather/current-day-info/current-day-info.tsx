5c687037a3e3cec93cba900776ed55d1e0841361
haythamIdrees9 <haythamidrees9@gmail.com>
2021-11-13 20:05:45 +0300

    fix ui 14 days message, and use real data instead of hardcoded for date

--- src/components/weather/current-day-info/current-day-info.tsx
+++ src/components/weather/current-day-info/current-day-info.tsx
@@ -1,6 +1,6 @@
 import "./current-day-info.scss"
 function CurrentDayInfo(props: { flipCards: boolean, currentDayData: any,loading:boolean }) {
-    const weekDays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+    const weekDays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
     const MonthsList = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     const currentDayDate = `${weekDays[new Date().getDay()]}, ${MonthsList[new Date().getMonth()]} ${new Date().getDate()}`;
 
