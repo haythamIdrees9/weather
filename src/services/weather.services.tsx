db8d60f81a84fb51a4df78746c263364632f7ba6
haythamIdrees9 <haythamidrees9@gmail.com>
2021-11-13 16:59:05 +0300

    add header to request

--- src/services/weather.services.tsx
+++ src/services/weather.services.tsx
@@ -1,7 +1,15 @@
 const WeatherService = {
 
     getWeatherData: function (city:string) {
-        return fetch(`http://api.weatherapi.com/v1/forecast.json?key=24258f8832a940d4839184700210711&q=${city}&days=10&aqi=no&alerts=no`)
+        return fetch(`http://api.weatherapi.com/v1/forecast.json?key=24258f8832a940d4839184700210711&q=${city}&days=10&aqi=no&alerts=no`, { 
+            method: 'get', 
+            headers: new Headers({
+                'Accept': 'application/json',
+              'Content-Type': 'application/json',
+              'Origin': '*',
+              'Host':'http://api.weatherapi.com'
+            }), 
+          })
     }
 
 
